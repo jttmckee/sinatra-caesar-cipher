a11a6de887d5ef5bd169774eb8b8181299a95d44
jttmckee <wp.jason@hotmail.co.uk>
2019-01-14 21:20:39 +0000

    Fixed encoding / decoding past a / z

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,6 @@
 def caesar_cipher(stringInput, shift)
   letters = stringInput.split('')
+  shift = shift % (26 * (shift.abs/shift))
   letters.collect! do |letter|
     if letter <= 'z' and letter >= 'a'
       letterNum = letter.ord
@@ -7,8 +8,13 @@ def caesar_cipher(stringInput, shift)
       letter = letterNum.chr
 
       if letter > 'z'
-        newShift = letter - 'z'.ord
-        letterNum = 'a'.ord + newShift
+        newShift = (letter.ord) - ('z'.ord)
+        letterNum = 'a'.ord + newShift - 1
+        letter = letterNum.chr
+      end
+      if letter < 'a'
+        newShift = ('a'.ord) - (letter.ord)
+        letterNum = 'z'.ord - newShift + 1
         letter = letterNum.chr
       end
     elsif letter <= 'Z' and letter >='A'
@@ -16,10 +22,14 @@ def caesar_cipher(stringInput, shift)
       letterNum += shift
       letter = letterNum.chr
       if letter > 'Z'
-        newShift = letter - 'Z'.ord
-        letterNum = 'A'.ord + newShift
+        newShift = (letter.ord) - ('Z'.ord)
+        letterNum = 'A'.ord + newShift - 1
         letter = letterNum.chr
-
+      end
+      if letter < 'A'
+        newShift = ('A'.ord) - (letter.ord)
+        letterNum = 'Z'.ord - newShift + 1
+        letter = letterNum.chr
       end
     end
      letter
